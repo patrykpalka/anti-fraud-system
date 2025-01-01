e0ab4ac21a583e8e732b8444b4cac545529df70f
patrykpalka <patryk-palka2@wp.pl>
2025-01-01 22:20:57 +0100

    Feat: Add app starting logging

--- src/main/java/antifraud/AntiFraudApplication.java
+++ src/main/java/antifraud/AntiFraudApplication.java
@@ -1,5 +1,7 @@
 package antifraud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,7 +11,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EnableCaching
 public class AntiFraudApplication {
+    private static final Logger logger = LoggerFactory.getLogger(AntiFraudApplication.class);
+
     public static void main(String[] args) {
+        logger.info("Starting Anti-Fraud System");
         SpringApplication.run(AntiFraudApplication.class, args);
+        logger.info("Anti-Fraud System started successfully");
     }
 }
